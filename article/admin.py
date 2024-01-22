0aba093d1a61eb5f26d91fc1962a55f1a4853137
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-01-22 11:25:39 -0800

    Coding model articles

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from . import models
 
-# Register your models here.
+
+# Admin panel customization for the Article
+@admin.register(models.ArticleCategory)
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'parent', 'is_active']
+    list_editable = ['parent', 'is_active']
+
+
+@admin.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'short_description', 'author', 'create_date', 'is_active']
+    list_editable = ['is_active']
--- article/models.py
+++ article/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+# Create models for Article.
+class ArticleCategory(models.Model):
+    parent = models.ForeignKey('ArticleCategory', null=True, blank=True, on_delete=models.CASCADE, verbose_name='دسته بندی والد')
+    title = models.CharField(max_length=200, verbose_name='عنوان دسته بندی')
+    url_title = models.CharField(max_length=200, unique=True, verbose_name='عنوان در url')
+    is_active = models.BooleanField(default=True, verbose_name='فعال / غیرفعال')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'دسته بندی مقاله'
+        verbose_name_plural = 'دسته بندی های مقاله'
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=300, verbose_name='عنوان مقاله')
+    slug = models.SlugField(max_length=400, db_index=True, allow_unicode=True, verbose_name='عنوان در url')
+    short_description = models.TextField(verbose_name='توضیحات کوتاه')
+    text = models.TextField(verbose_name='متن مقاله')
+    selected_categories = models.ManyToManyField(ArticleCategory, verbose_name='دسته بندی ها')
+    image = models.ImageField(upload_to='images/articles', verbose_name='تصویر مقاله')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, editable=False, verbose_name='نویسنده')
+    create_date = models.DateTimeField(auto_now_add=True, editable=False, verbose_name='تاریخ ثبت')
+    is_active = models.BooleanField(default=True, verbose_name='فعال / غیرفعال')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقالات'
