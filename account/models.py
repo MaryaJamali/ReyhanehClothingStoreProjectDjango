6c3424d8b6738b5ebb5581374ea27a5837187fdf
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-01-23 08:35:30 -0800

    Add a field to the user model

--- account/models.py
+++ account/models.py
@@ -9,11 +9,15 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=20, null=True, blank=True,  verbose_name='تلفن همراه')
     avatar = models.CharField(max_length=20, null=True, blank=True, verbose_name='تصویر آواتار')
     email_active_code = models.CharField(max_length=100, verbose_name='کد فعالسازی ایمیل')
+    about_user = models.TextField(null=True, blank=True, verbose_name='درباره شخص')
+    address = models.TextField(null=True, blank=True, verbose_name='آدرس')
 
+    def __str__(self):
+        if self.first_name is not '' and self.last_name is not '':
+            return self.get_full_name()
+
+        return self.email
 
     class Meta:
         verbose_name = 'کاربر'
         verbose_name_plural = 'کاربران'
-
-    def __str__(self):
-        return self.get_full_name()
