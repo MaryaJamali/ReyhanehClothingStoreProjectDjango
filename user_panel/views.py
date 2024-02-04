1bb4a14d9a04f4ad3553c7070bb605d131248471
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-04 03:13:57 -0800

    Create menu component

--- user_panel/views.py
+++ user_panel/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 from django.views.generic import TemplateView
 
 
 # Class_base_Template_View for User_panel page
 class UserPanelDashboardPage(TemplateView):
     template_name = 'user_panel/user_panel_dashboard_page.html'
+
+
+# Function_base_View for User_panel_menu
+def user_panel_menu_component(request: HttpRequest):
+    return render(request, 'user_panel/include/user_panel_menu_component.html')
