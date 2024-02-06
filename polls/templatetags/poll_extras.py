d11ce0529141ffac68a118c6b9aae46a7b6d3974
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-06 11:50:54 -0800

    Writing a tag template to multiply the price and quantity

--- polls/templatetags/poll_extras.py
+++ polls/templatetags/poll_extras.py
@@ -8,3 +8,9 @@ register = template.Library()
 def three_digits_currency(value: int):
     return '{:,}'.format(value) + ' تومان'
 
+
+# Writing a tag template to multiply the price of a single product by its number
+@register.simple_tag
+def multiply(quantity, price, *args, **kwargs):
+    return three_digits_currency(quantity * price)
+
