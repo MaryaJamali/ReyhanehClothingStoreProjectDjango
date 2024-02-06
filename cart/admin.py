a0be804b8f3c186f854bd581ff4cd9c3ea756100
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-06 05:11:34 -0800

    Coding part of the shopping cart model

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+# Admin panel customization for the cart
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'payment_date', 'is_paid']
+
+
+@admin.register(models.OrderDetail)
+class OrderDetailAdmin(admin.ModelAdmin):
+    list_display = ['product', 'count', 'final_price']
--- cart/urls.py
+++ cart/urls.py
@@ -17,5 +17,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(''),
 ]
