34575c6fd4f7ca9bf292cce4139d794b5746e822
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-09 19:36:26 +0330

    Code modification and testing

--- product/urls.py
+++ product/urls.py
@@ -18,10 +18,10 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='product_list_page_view'),
-    path('cat/<str:category>/', views.ProductListView.as_view(), name='product_list_by_category'),
-    path('brand/<str:brand>/', views.ProductListView.as_view(), name='product_list_by_brands'),
-    path('size/<int:size>/', views.ProductListView.as_view(), name='product_list_by_size'),
-    path('color/<str:color>/', views.ProductListView.as_view(), name='product_list_by_color'),
+    path('cat/<str:category>', views.ProductListView.as_view(), name='product_list_by_category'),
+    path('brand/<str:brand>', views.ProductListView.as_view(), name='product_list_by_brands'),
+    path('size/<int:size>', views.ProductListView.as_view(), name='product_list_by_size'),
+    path('color/<str:color>', views.ProductListView.as_view(), name='product_list_by_color'),
     # Slug is the dynamic and changeable part ---> Domain name/posts/slug
     path('<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail_page_view'),
     path('add-product-comment', views.add_product_comment, name='add_product_comment')
