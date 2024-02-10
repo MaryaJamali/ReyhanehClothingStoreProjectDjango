6958dc84191e8244a62863680a2d729cd70de573
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-10 15:51:37 +0330

    Change form to model form

--- base/forms.py
+++ base/forms.py
@@ -1,23 +1,34 @@
 from django import forms
 from django.core import validators
+from .models import NewsletterSubscription
 
 
-# Class_Form for NewsletterSubscriptionForm Form
-class NewsletterSubscriptionForm(forms.Form):
-    email = forms.EmailField(
-        label='ایمیل *',
+# Class_Form for NewsletterSubscription Form
+class NewsletterSubscriptionModelForm(forms.ModelForm):
+    class Meta:
+        # The created form should be directly connected to the database
+        model = NewsletterSubscription
+        # Display fields for the user
+        fields = ['email']
         # A widget is a simple shortcode that allows you to add features to your website or blog.and we can
         # with widgets manipulate the appearance of Html forms a bit
-        widget=forms.EmailInput(
-            attrs={
+        widgets = {
+            'email': forms.TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'username@mail.com',
+                'placeholder': 'username@mail.com'
             }),
-        error_messages={
-            'required': 'لطفا ایمیل خود را وارد کنید',
-        },
-        validators=[
+        }
+        labels = {
+            'email': 'ایمیل*',
+        }
+        # error_messages={} : Managing the error text
+        # required : It can be used to specify that an input field in the form must be completed.
+        error_messages = {
+            'email': {
+                'required': 'لطفا ایمیل خود را وارد کنید'
+            },
+        }
+        validators = [
             validators.MaxLengthValidator(100),
-            validators.EmailValidator,
+            validators.EmailValidator
         ]
-    )
