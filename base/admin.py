075dce1edd587f58a32d10ed43aa4ec02bc19abf
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-10 15:04:39 +0330

    Creating a newsletter subscription model

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+# Admin panel customization for the NewsletterSubscriber
+@admin.register(models.NewsletterSubscriber)
+class NewsletterSubscriberAdmin(admin.ModelAdmin):
+    # Display fields on the Admin page
+    list_display = ['user', 'email', 'create_date', 'is_active']
+    # Changing the fields on the Admin page
+    list_editable = ['is_active']
--- base/models.py
+++ base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+# Create models for NewsletterSubscriber.
+class NewsletterSubscriber(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+    email = models.EmailField(unique=True, max_length=300, verbose_name='ایمیل')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ثبت')
+    is_active = models.BooleanField(default=True, verbose_name='فعال / غیرفعال')
+
+    def __str__(self):
+        return str(self.user)
+
+    class Meta:
+        verbose_name = 'ایمیل اشتراک در خبرنامه'
+        verbose_name_plural = 'ایمیل های اشتراک در خبرنامه'
