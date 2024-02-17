46ab79ad212be92fc4ac66a0cee81ae9ff3eec4c
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-17 14:52:35 +0330

    Addressing for the payment gateway

--- cart/urls.py
+++ cart/urls.py
@@ -18,4 +18,7 @@ from . import views
 
 urlpatterns = [
     path('add-to-cart', views.add_product_to_order, name='add_product_to_order_view'),
+    # Addressing for the payment gateway
+    path('request-payment', views.request_payment, name='request_payment'),
+    path('verify-payment', views.verify_payment, name='verify_payment'),
 ]
