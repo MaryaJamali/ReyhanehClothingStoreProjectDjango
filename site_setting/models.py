0797582293462a7332ba981185108fb1e1851689
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-21 23:59:40 +0330

    Add new field

--- site_setting/models.py
+++ site_setting/models.py
@@ -105,6 +105,7 @@ class QuestionCommon(models.Model):
     question = models.CharField(max_length=200, verbose_name='سوال')
     response = models.URLField(max_length=500, verbose_name='پاسخ')
     question_common_group = models.ForeignKey(to=QuestionCommonGroup, on_delete=models.CASCADE, verbose_name='دسته بندی')
+    is_active = models.BooleanField(default=True, verbose_name='فعال / غیرفعال')
 
     def __str__(self):
         return self.question
